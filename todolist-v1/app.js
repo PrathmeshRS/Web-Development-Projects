fc8a299d05edf763d19fa8c6daacd44549616e50
Prathmesh S <shindeprathmesh99@gmail.com>
2020-08-07 11:02:43 +0530

    Finish project

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,23 +16,29 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-    
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
+    const day = date.getDate();
 
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render('list', {kindOfDay: day, newTasks : items});
+    res.render('list', {listTitle: day, newTasks : items});
 });
+
+app.get("/work", function(req, res){
+    res.render('list', {listTitle:"Work List", newTasks : workItems});
+});
+
+app.get("/about", function(req, res) {
+    res.render('about')
+})
 
 app.post("/", function(req, res){
     let item = req.body.task;
+    if(req.body.list == "Work List") {
+        
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
     items.push(item);
     res.redirect("/");
+    }
 });
 
 app.listen(3000, function(req, res){
