90d8af3c25c367db988453e51dd73f519eabd974
Prathmesh S <shindeprathmesh99@gmail.com>
2020-09-25 11:27:55 +0530

    Complete level-3 security.

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,11 +1,13 @@
 //jshint esversion:6
 // dotenv package for security
-require('dotenv').config();
+// require('dotenv').config();
+// We'll use md5 hashing
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption'); 
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5'); 
 
 const app = express();
 
@@ -24,7 +26,7 @@ const userSchema = new mongoose.Schema({
 
 // The salt for encryption
 // The database will be encrypted, but only the password field will be encrypted. 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +47,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -67,7 +69,7 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if(foundUser) {
-                if (foundUser.password === password) {
+                if (foundUser.password === md5(password)) {
                     res.render("secrets");
                 } else {
                     res.redirect("/login");
